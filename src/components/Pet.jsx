7af96f2913445a76bda17326d4e02c9bb7453de1
Steve Bazelais <57912340+SBazelais@users.noreply.github.com>
2022-04-28 14:48:06 -0400

    api call function

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -1,16 +1,12 @@
-export const Pet = ({ animalData }) => {
+export const Pet = ({ name, breed, animal }) => {
   return (
     <>
-      {animalData.map(({ name, breed, animal }) => {
-        return (
-          <div key={name}>
-            <h1>Name: {name}</h1>
-            <h2>Breed: {breed}</h2>
-            <h2>Animal: {animal}</h2>
-            <br />
-          </div>
-        );
-      })}
+      <div key={name}>
+        <h1>Name: {name}</h1>
+        <h2>Breed: {breed}</h2>
+        <h2>Animal: {animal}</h2>
+        <br />
+      </div>
     </>
   );
 };
--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Pet } from "./Pet";
 
-const ANIMAL = ["Dog", "Bird", "Reptile"];
+const ANIMAL = ["Bird", "Cat", "Dog", "Rabbit", "Reptile"];
 
 export const SearchParams = () => {
-  const [location, setLocation] = useState("Boston, MA");
+  const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
   const breeds = [];
+
+  useEffect(() => {
+    requestPets();
+  }, []); //eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets() {
+    const res = await fetch(
+      `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
@@ -65,6 +79,14 @@ export const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
